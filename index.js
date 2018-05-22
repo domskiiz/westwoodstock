33b214283ae93407267e1256a3b56da8bc1cf1fb
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-05-22 14:20:30 -0700

    logo and artists basic layout

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+var express = require('express');
+var path = require('path');
+// var favicon = require('serve-favicon');
+
+var app = express();
+
+// Serve static files (CSS, images, js files, etc)
+app.use(express.static(path.join(__dirname, '/public')));
+// Set view engine to HTML so app can render html files
+app.engine('html', require('ejs').renderFile);
+// Serve favicon
+// app.use(favicon(path.join(__dirname + '/resources/assets/favicon.ico')));
+
+// Routes
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+app.get('*', function(req, res) {
+  res.send('404: page not found');
+});
+
+app.listen(process.env.PORT || 3000);
