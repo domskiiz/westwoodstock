21317cbbf1165ead40d675e114e3053b8a09a245
Alissa Niewiadomski <alissaaadomski@gmail.com>
2018-05-25 21:02:54 -0700

    configured for github pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# westwoodstock
-Westwoodstock website
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,14 @@
 $( document ).ready(function() {
   if($(window).width() <= 630) {
     $('.links').addClass("flex-col");
-    $('.sponsors').addClass("flex-col");
   }
 
   $( window ).resize(function() {
     if($(window).width() <= 630) {
       $('.links').addClass("flex-col");
-      $('.sponsors').addClass("flex-col");
     }
     else {
       $('.links').removeClass("flex-col");
-      $('.sponsors').removeClass("flex-col");
     }
   });
 })
